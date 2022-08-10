e2cab1c24a371a1b34290fc138c1edaed9a7a0a4
Cindiddy <101303125+Cindiddy@users.noreply.github.com>
2022-08-10 14:49:46 -0400

    add sqlSession Factory utils class

---
+++ LoginDemo/src/main/java/com/cindy/utils/SqlSessionFactoryUtils.java
@@ -0,0 +1,25 @@
+package com.cindy.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SqlSessionFactoryUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+    static{
+        String resource = "mybatis-config.xml";
+        try (InputStream inputStream = Resources.getResourceAsStream(resource)) {
+            sqlSessionFactory =
+                    new SqlSessionFactoryBuilder().build(inputStream);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static  SqlSessionFactory getSqlSessionFactory(){
+        return sqlSessionFactory;
+    }
+}
